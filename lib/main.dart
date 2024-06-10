776f904b67515aac3b582d7a40c3d7293020241d
Mohammad Hojjat <hojjatjanjan@gmail.com>
2024-06-10 23:12:27 +0430

    last changes made!

--- lib/main.dart
+++ lib/main.dart
@@ -26,18 +26,27 @@ class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _lastNameController = TextEditingController();
   final TextEditingController _addressController = TextEditingController();
 
-  String _fullName = '';
-  String _lastName = '';
-  String _address = '';
+  List<Map<String, String>> _submittedData = [];
 
   void _submitForm() {
     if (_formKey.currentState!.validate()) {
       setState(() {
-        _fullName = _fullNameController.text;
-        _lastName = _lastNameController.text;
-        _address = _addressController.text;
+        _submittedData.add({
+          'fullName': _fullNameController.text,
+          'lastName': _lastNameController.text,
+          'address': _addressController.text,
+        });
+        _fullNameController.clear();
+        _lastNameController.clear();
+        _addressController.clear();
       });
     }
+  }
+
+  void _deleteItem(int index) {
+    setState(() {
+      _submittedData.removeAt(index);
+    });
   }
 
   @override
@@ -48,67 +57,93 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                controller: _fullNameController,
-                decoration: InputDecoration(
-                  labelText: 'Full Name',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your full name';
-                  }
-                  return null;
+        child: Column(
+          children: <Widget>[
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    controller: _fullNameController,
+                    decoration: InputDecoration(
+                      labelText: 'Full Name',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your full name';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: _lastNameController,
+                    decoration: InputDecoration(
+                      labelText: 'Last Name',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your last name';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    controller: _addressController,
+                    decoration: InputDecoration(
+                      labelText: 'Address',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your address';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: _submitForm,
+                    child: Text('Submit'),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 24),
+            Text(
+              'Submitted Values:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _submittedData.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    margin: EdgeInsets.symmetric(vertical: 8.0),
+                    child: ListTile(
+                      title: Text(
+                          'Full Name: ${_submittedData[index]['fullName']}'),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                              'Last Name: ${_submittedData[index]['lastName']}'),
+                          Text('Address: ${_submittedData[index]['address']}'),
+                        ],
+                      ),
+                      trailing: IconButton(
+                        icon: Icon(Icons.delete),
+                        onPressed: () => _deleteItem(index),
+                      ),
+                    ),
+                  );
                 },
               ),
-              SizedBox(height: 16),
-              TextFormField(
-                controller: _lastNameController,
-                decoration: InputDecoration(
-                  labelText: 'Last Name',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your last name';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                controller: _addressController,
-                decoration: InputDecoration(
-                  labelText: 'Address',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your address';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: _submitForm,
-                child: Text('Submit'),
-              ),
-              SizedBox(height: 24),
-              Text(
-                'Submitted Values:',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 8),
-              Text('Full Name: $_fullName'),
-              Text('Last Name: $_lastName'),
-              Text('Address: $_address'),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
